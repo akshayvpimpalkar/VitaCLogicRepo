776f0eccea31fe02f33f44ff44f99ac922d026d9
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2017-09-09 00:59:55 +0530

    star pyramid with alternate start and space printing

--- StarPyramid/StarPyramid/StarPyramid.cpp
+++ StarPyramid/StarPyramid/StarPyramid.cpp
@@ -16,11 +16,29 @@ int main()
 	c = 2 * rows - 1;
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < c; j++) {
-			if (j >= (c / 2) - i && j <= (c / 2) + i) {
+			if (j >= (rows - 1 - i) && j <= (rows - 1 + i)) {
 				cout << "*";
 			}
 			else {
 				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << "------------------------------------" << endl;
+
+	int k; //to toggle k to print alternate "*" with " " in each row
+	for (int i = 0; i < rows; i++) {
+		k = 1;
+		for (int j = 0; j < c; j++) {
+			if (j >= (rows - 1 - i) && j <= (rows - 1 + i) && k) {
+				cout << "*";
+				k = 0;
+			}
+			else {
+				cout << " ";
+				k = 1;
 			}
 		}
 		cout << endl;
