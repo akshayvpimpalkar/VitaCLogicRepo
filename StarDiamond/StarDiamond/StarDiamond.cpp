03c52ac0ffcbadf972037f01cc56753b62ae61a3
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2017-09-09 18:07:06 +0530

    now it can work with even values of rows and 2nd alternate method added

--- StarDiamond/StarDiamond/StarDiamond.cpp
+++ StarDiamond/StarDiamond/StarDiamond.cpp
@@ -14,18 +14,66 @@ int main()
 
 	c = rows; //rows and columns are same
 	int k = 0; //this to vary the value of subtraction before and after the middle of diamond
+
+	int n;
+	if (rows % 2 == 0) { 
+		n = (rows / 2) - 1;
+	}
+	else {
+		n = rows / 2; // its the variable to indicate middle row value
+	}
+	 
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < c; j++) {
-			if (j >= (rows/2 - k) && j <= (rows/2 + k)) {
+			if (j >= (n - k) && j <= (n + k)) {
 				cout << "*";
 			}
 			else {
 				cout << " ";
 			}
 		}
-		i < (rows/2) ? k++ : k--; //condition to vary the value of subtraction
+
+		if (rows % 2 == 0) {
+			if (i < n) k++;
+			if (i > n) k--;
+		}
+		else {
+			i < n ? k++ : k--; //condition to vary the value of subtraction
+		}
+		
 		cout << endl;
 	}
-    return 0;
+    
+	cout << "--------------------------------------------" << endl;
+
+	/*
+	The below is same but just using indeces for i,j from 1 to number of rows 
+	and columns respectively
+	*/
+	k = 0; //we have to reset k because we have changed it's value in above pattern
+
+	int n1 = (rows + 1) / 2; //n1 is the middle index of the column for this pattern
+	for (int i = 1; i <= rows; i++) {
+
+		if (rows % 2 == 0) {
+			if (i <= n1) k++;
+			if (i > (n1+1)) k--;
+		}
+		else {
+			i <= n1 ? k++ : k--; //condition to vary the value of subtraction
+		}
+		for (int j = 1; j <= c; j++) {
+			if (j >= (n1+1 - k) && j <= (n1-1 + k)) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+
+
+	return 0;
 }
 
